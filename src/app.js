55dbaa6c48579183b341d802e1808b4dcd22f741
Altus24 <116025673+Altus24@users.noreply.github.com>
2023-02-27 10:29:28 -0300

    modificaciones de express

--- src/app.js
+++ src/app.js
@@ -12,3 +12,8 @@ app.get('/', function(req,res){
 const port = process.env.PORT || 3030;
 
 app.listen(port, ()=> console.log ('Servidor corriendo en el puerto ' + port));
+
+
+
+//npm start de package.json es para correr en render
+//el gitignore hay q hacerlo antes del primer add

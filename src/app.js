0468b7e28b8e9ccce99a80aec4e3da7ffa30c88f
Altus24 <116025673+Altus24@users.noreply.github.com>
2023-02-28 00:52:19 -0300

    Commit 3

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require ('express');
 const app = express();
 const path = require('path');
  
-//Recursos publics
+//Recursos publics estatics. PAra qu carguen mas rapido
 app.use(express.static(path.resolve(__dirname,'../public')));
 
 app.get('/', function(req,res){
     res.sendFile(path.resolve(__dirname,'./view/index.html'))
 })
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3030;  
 
 app.listen(port, ()=> console.log ('Servidor corriendo en el puerto ' + port));
 
